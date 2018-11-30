dc8694398af8430b56bd0dbe17da5a5fba407718
Joshua Adams <joshadams554@gmail.com>
2018-11-30 15:37:54 -0500

    refactor training

--- src/component/training.js
+++ src/component/training.js
@@ -1,3 +1,5 @@
+//import statements (React Component connect grader saveUserData training.css notes.css 
+//fetchHoldCard, fetchIdealCards, fetchHand, updateGame, updateNote, resetGame)
 import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
@@ -9,21 +11,17 @@ import './notes.css';
 
 import { fetchHoldCard, fetchIdealCards, fetchHand, updateGame, updateNote, resetGame } from '../actions/game';
 
-// import Deck from "react-poker";
+// build deck object from cards dependency
 const { decks } = require('cards');
+
+//set feedback variables
 let score = 0;
 let handsPlayed = 0;
 let correctGuess = 0;
 
-const gradeTheHand = function (hand) {
-
-  return grader(hand);
-}
-
-
-
 class Training extends Component {
 
+  //local props
   constructor(props) {
     super(props);
     this.state = {
@@ -31,9 +29,21 @@ class Training extends Component {
     };
   }
 
+  //on mount dealHand
   componentDidMount() {
     this.dealHand();
   }
+
+  //deal the hand makes new deck shuffles it and draws 5 cards then dispatches to the state
+  dealHand() {
+    const deck = new decks.StandardDeck({ jokers: 0 });
+    deck.shuffleAll();
+    const hand = deck.draw(5);
+    this.props.dispatch(fetchHand(hand));
+  }
+
+  //main render function
+  //calls renderTheBoard grade, saveGame, dealHand, showNotes, renderNotes
   render() {
     const board = this.renderTheBoard(this.props.cards);
     const confirmButton = (<button onClick={() => this.grade(this.props.cards)}>Confirm</button>);
@@ -56,6 +66,48 @@ class Training extends Component {
 
   }
 
+  //renders the baord
+  //calls handRender
+  renderTheBoard(hand) {
+    return (
+      <div className="game-component" >
+        <ul
+        >
+          {this.handRender(hand)}
+        </ul>
+      </div>
+    )
+  }
+
+
+  //dispatches the current hand to the grader calls fetchIdealCards and grader, calculateScore and updateGame
+  grade(cards) {
+    this.props.dispatch(fetchIdealCards(grader(cards)));
+    handsPlayed++;
+    this.calculateScore(cards);
+    this.props.dispatch(updateGame(handsPlayed, score));
+  }
+
+  //save game saves the current game state to the user history and resets the game
+  //calls save UserData then resetsGame, then dealHand
+  saveGame() {
+    this.props.dispatch(saveUserData(this.props.game))
+      .then(this.props.dispatch(resetGame()))
+      .then(this.dealHand())
+      .then(
+        score = 0,
+        handsPlayed = 0,
+        correctGuess = 0
+      )
+  }
+
+  //shows the notes modal via local state
+  showNotes() {
+    this.setState({ showModal: true })
+  }
+
+  //renders the note controls and field
+  //calls updateTheNote and setState
   renderNotes() {
     return (
       <div className="noteModal">
@@ -70,12 +122,16 @@ class Training extends Component {
     )
   }
 
-  grade(cards) {
-    this.props.dispatch(fetchIdealCards(gradeTheHand(cards)));
-    handsPlayed++;
-    this.calculateScore(cards);
-    this.props.dispatch(updateGame(handsPlayed, score));
-  }
+  //renders the hand itself
+  //calls heldCard and openCard
+  handRender(hand) {
+    return hand.map((card, i) => {
+      return (card.held ? this.heldCard(card, i) : this.openCard(card, i))
+    }
+    )
+  };
+
+  //calculates the score of current training session
   calculateScore(hand) {
     let counter = 0;
     for (let i = 0; i < hand.length; i++) {
@@ -89,36 +145,13 @@ class Training extends Component {
     score = correctGuess / handsPlayed;
   }
 
-  renderTheBoard(hand) {
-    return (
-      <div className="game-component" >
-        <ul
-
-        >
-          {this.deckRender(hand)}
-        </ul>
-
-
-      </div>
-    )
-  }
-
-  showNotes() {
-    this.setState({ showModal: true })
-  }
-
+  //updates the note state
   updateTheNote(value) {
     this.props.dispatch(updateNote(value))
   }
 
-  deckRender(hand) {
-    return hand.map((card, i) => {
-      return (card.held ? this.heldCard(card, i) : this.openCard(card, i))
-    }
-    )
-  };
-
-
+  //renders each card that has been set to held
+  //dispatches fetchHoldCard
   heldCard(card, i) {
     const imageName = card.rank + (card.suit.charAt(0).toUpperCase());
     const image = (`/images/JPEG/${imageName}.jpg`)
@@ -142,6 +175,8 @@ class Training extends Component {
     }
   }
 
+  //renders each card that has not been set to held
+  //dispatches fetchHoldCard
   openCard(card, i) {
     const imageName = card.rank + (card.suit.charAt(0).toUpperCase());
     const image = (`/images/JPEG/${imageName}.jpg`)
@@ -164,27 +199,9 @@ class Training extends Component {
       </li>
     }
   }
-
-
-  saveGame() {
-    this.props.dispatch(saveUserData(this.props.game))
-      .then(this.props.dispatch(resetGame()))
-      .then(this.dealHand())
-      .then(
-        score = 0,
-        handsPlayed = 0,
-        correctGuess = 0
-      )
-  }
-
-  dealHand() {
-    const deck = new decks.StandardDeck({ jokers: 0 });
-    deck.shuffleAll();
-    const hand = deck.draw(5);
-    this.props.dispatch(fetchHand(hand));
-  }
 }
 
+//maps the game cards and notes to state
 const mapStateToProps = (state) => {
   return ({
     game: state.game,
