56ee039f7b97238cb60d57ed93d78029f2ce0016
Joshua Adams <joshadams554@gmail.com>
2018-12-04 15:11:13 -0500

    jacks or better to dos finished logic to be implimented later

--- src/jacksOrBetter.js
+++ src/jacksOrBetter.js
@@ -1,6 +1,53 @@
 export const jacksOrBetter = (input) => {
   //:TODO
-  //return the array of objects that should be held based on input array of objects
+
+  //figure out what type of hand it is based on strategy
+  
+
+  //return the array of objects that should be held based on above result
 
   return input;
 }
+
+
+
+//jacks or better strategy
+/*----------------------TO DO-------------------------/
+Hand Type, (expected return)
+Dealt royal flush (800.0000)
+Dealt straight flush (50.0000)
+Dealt four of a kind (25.0000)
+4 to a royal flush (18.3617)
+Dealt full house (9.0000)
+Dealt flush (6.0000)
+3 of a kind (4.3025)
+Dealt straight (4.0000)
+4 to a straight flush (3.5319)
+Two pair (2.59574)
+High pair (1.5365)
+3 to a royal flush (1.2868) A
+4 to a flush (1.2766)
+Unsuited TJQK(0.8723)
+Low pair (0.8237)
+4 to an outside straight with 0-2 high cards(0.6809)
+3 to a straight flush (type 1) (0.6207 to 0.6429)
+Suited QJ (0.6004)B
+4 to an inside straight, 4 high cards (0.5957)
+Suited KQ or KJ (0.5821)
+Suited AK, AQ, or AJ (0.5678)
+4 to an inside straight, 3 high cards (0.5319)
+3 to a straight flush (type 2) (0.5227 to 0.5097)C
+Unsuited JQK (0.5005)
+Unsuited JQ (0.4980)
+Suited TJ (0.4968) D
+2 unsuited high cards king highest (0.4862)
+Suited TQ (0.4825) E
+2 unsuited high cards ace highest (0.4743)
+J only (0.4713)
+Suited TK (0.4682) F
+Q only (0.4681)
+K only (0.4649)
+A only (0.4640)
+3 to a straight flush (type 3) (0.4431)
+Garbage, discard everything (0.3597)
+---------------------------------------------------------*/
