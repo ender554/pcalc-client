5c17ab0e5e4208862c0de226ce3287f4e84e488a
Joshua Adams <joshadams554@gmail.com>
2018-12-04 13:06:11 -0500

    styled splash

--- src/component/splash.js
+++ src/component/splash.js
@@ -5,15 +5,19 @@ import './splash.css';
 export function Splash(){
   return(<main className="splash-page">
   <div className="focus">
-    <h1>PCALC</h1>
-    <p className="about">Contrary to popular belief, Not all casino games are negative equity!
+    <h1 className="bigLogo"><img src={"images/pcalcclean.png"} alt='logo' /></h1>
+    <div>
+  <p className="about">Contrary to popular belief, Not all casino games are negative equity!
     Some forms of video poker have positive equity! If played correctly!
     This app is to give you that perfect strategy!
     </p>
-    <p>Instructions:</p>
+  <p>Instructions:</p>
     <p><Link to='/game'><button>New Game</button></Link> allows you to enter in cards and recieve the cards to hold</p>
     <p><Link to='/training'><button>Training</button></Link> play fast paced guesswork game to see how good you are</p>
+  
   </div>
+  </div>
+  
 </main>
   )
 }
